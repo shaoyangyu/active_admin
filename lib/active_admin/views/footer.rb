fb3d66f2b3efda8f9cabae75f3bd8dcde408447a
shaoyang <shaoyang.yu@fairfaxmedia.com.au>
2017-05-11 12:03:36 +1000

    change footer

--- lib/active_admin/views/footer.rb
+++ lib/active_admin/views/footer.rb
@@ -11,7 +11,7 @@ module ActiveAdmin
 
       def powered_by_message
         para I18n.t('active_admin.powered_by',
-          active_admin: link_to("Active Admin", "http://www.activeadmin.info"),
+          active_admin: link_to("Health Care", ""),
           version: ActiveAdmin::VERSION).html_safe
       end
 
